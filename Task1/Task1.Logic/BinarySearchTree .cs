3e70f2edb58e87b4f9d196fceff39d8a0906b799
Domino <zhikhanastasya@gmail.com>
2018-07-25 19:30:46 +0300

    Add files

---
+++ Task1/Task1.Logic/BinarySearchTree .cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Logic
+{
+    public sealed class BinarySearchTree<T>
+    {
+        private Node<T> _head;
+
+        public BinarySearchTree()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BinarySearchTree(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Preorder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Inorder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Postorder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Node<T> node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task1/Task1.Logic/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Logic
+{
+    public sealed class Node<T>
+    {
+        public T data { get; set; }
+
+        public Node<T> Left { get; set; }
+
+        public Node<T> Right { get; set; }
+    }
+}
