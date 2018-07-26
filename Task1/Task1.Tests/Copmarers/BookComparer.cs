aec847b2317303f1d05fd5ae563b048016cc1c29
Domino <zhikhanastasya@gmail.com>
2018-07-26 23:37:05 +0300

    Add file

---
+++ Task1/Task1.Tests/Copmarers/BookComparer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.Tests.Additional_types;
+
+namespace Task1.Tests.Copmarers
+{
+    class BookComparer : IComparer<Book>
+    {
+        /// <summary>
+        /// Compare two books by number of pages
+        /// </summary>
+        /// <param name="left"> First book to compare </param>
+        /// <param name="right"> Srcond book to compare </param>
+        /// <returns> 
+        /// A signed integer that indicates the relative values of left and right, as shown in the
+        /// following table.Value Meaning Less than zero left is less than right.Zero left equals right.Greater
+        /// than zero left is greater than right. 
+        /// </returns>
+        public int Compare(Book left, Book right)
+        {
+            if (left == null && right == null)
+            {
+                return 0;
+            }
+
+            if (left == null)
+            {
+                return -1;
+            }
+
+            if (right == null)
+            {
+                return 1;
+            }
+
+            if (left.Pages < right.Pages)
+            {
+                return -1;
+            }
+
+            if (left.Pages> right.Pages)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
