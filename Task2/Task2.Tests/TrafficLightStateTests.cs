55d6d95424f00a6ecff1c06d0a2fa47691a1efc5
Domino <zhikhanastasya@gmail.com>
2018-07-27 20:42:30 +0300

    Add (Task.Tests) simple tests for TrafficManager.Start

--- Task2/Task2.Tests/TrafficLightStateTests.cs
+++ Task2/Task2.Tests/TrafficLightStateTests.cs
@@ -7,14 +7,29 @@ namespace Task2.Tests
     class TrafficLightStateTests
     {
         #region Test data
-
+        static object[] _initStates =
+        {
+            new object[] { new RedState(), 1,
+                new ColourOption[] { ColourOption.Red } },
+            new object[] { new RedState(), 2,
+                new ColourOption[] { ColourOption.Red, ColourOption.RedAndYellow }},
+            new object[] { new RedState(), 4,
+                new ColourOption[] { ColourOption.Red, ColourOption.RedAndYellow, ColourOption.Green, ColourOption.Yellow }},
+             new object[] { new RedYellowState(), 6,
+                 new ColourOption[] { ColourOption.RedAndYellow, ColourOption.Green, ColourOption.Yellow, ColourOption.Red, ColourOption.RedAndYellow, ColourOption.Green} },
+        };
         #endregion
 
-        [Test]
-        public void ChangeState()
+        [Test, TestCaseSource("_initStates")]
+        public void ChangeState(ILightState state, int changesCount, ColourOption[] colours)
         {
-            TrafficLightState trafficLight = new TrafficLightState(new RedState());
-            trafficLight.Change();
+            var actual = TrafficManager.Start(state, changesCount);
+
+            int i = 0;
+            foreach (var element in actual)
+            {
+                Assert.AreEqual(element, colours[i++]);
+            }
         }
     }
 }
