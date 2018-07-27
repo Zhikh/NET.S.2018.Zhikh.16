220467aaeb240ce9c69056582303250786ba14c5
Domino <zhikhanastasya@gmail.com>
2018-07-27 20:41:03 +0300

    Add file

---
+++ Task2/Task2.Logic/State pattern/TrafficManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Task2.Logic.StatePattern;
+
+namespace Task2.Logic.StatePattern
+{
+    public static class TrafficManager
+    {
+        /// <summary>
+        /// Run cycle of change states fo traffic light
+        /// </summary>
+        /// <param name="initState"> Init state </param>
+        /// <param name="shiftsNumber"> Number of changes states </param>
+        /// <returns> Collection of colours </returns>
+        /// <exception cref="ArgumentNullException"> If initSata is null </exception>
+        public static IEnumerable<ColourOption> Start(ILightState initState, int shiftsNumber)
+        {
+            var trafficLight = new TrafficLightState(initState);
+
+            for (int i = 0; i < shiftsNumber; i++)
+            {
+                yield return trafficLight.Colour;
+
+                trafficLight.Change();
+            }
+        }
+    }
+}
