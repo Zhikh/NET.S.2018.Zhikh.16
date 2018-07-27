3555a55254ce651ceb2e19d7ea0bae54d4634ca8
Domino <zhikhanastasya@gmail.com>
2018-07-28 00:09:48 +0300

    Add files

---
+++ Task1/Task1.Tests/Additional types/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace Task1.Tests.AdditionalTypes
+{
+    public struct Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Task1/Task1.Tests/Copmarers/PointComparer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Task1.Tests.AdditionalTypes;
+
+namespace Task1.Tests.Copmarers
+{
+    public sealed class PointComparer : IComparer<Point>
+    {
+        public int Compare(Point left, Point right)
+        {
+            int leftSum = left.X + left.Y;
+            int rightSum = right.X + right.Y;
+
+            if (leftSum < rightSum)
+            {
+                return -1;
+            }
+
+            if (leftSum > rightSum)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
